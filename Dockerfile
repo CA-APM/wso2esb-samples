e6d79bfe993c2390dc7ae5719306cbe0d3d3b02b
Guenter Grossberger <guenter.grossberger@ca.com>
2019-03-06 10:32:55 +0000

    minor updates, optionally use sample number in docker-compose/run_client.sh

--- Dockerfile
+++ Dockerfile
@@ -3,19 +3,20 @@ FROM wso2esb:4.5.0
 ENV CA_AGENT=true \
     MANAGER_URL=apm-docker:5009
 
-COPY startup.sh run_client.sh wso2esb.tar.gz ./
-
-ADD IntroscopeAgentFiles-NoInstaller10.7.0.90tomcat.unix.tar .
-
 # install ant and curl, build SimpleStockQuoteService
 RUN rm -rf /var/cache/apk/* /tmp/* && \
     apk update && \
     apk add curl apache-ant && \
     rm -rf /var/cache/apk/* /tmp/* && \
-    chmod +x startup.sh run_client.sh && \
     cd samples/axis2Server/src/SimpleStockQuoteService && \
     ant
 
 EXPOSE 9000
+
+ADD IntroscopeAgentFiles-NoInstaller10.7.0.90tomcat.unix.tar .
+
+COPY startup.sh run_client.sh wso2esb.tar.gz ./
+
+RUN chmod +x startup.sh run_client.sh
 
 ENTRYPOINT ["./startup.sh"]
--- run_client.sh
+++ run_client.sh
@@ -1,11 +1,32 @@
 #!/bin/sh
 
 # let servers start
-sleep 10
+STARTING=1
+while true; do
+    grep "WSO2 Carbon started" repository/logs/wso2carbon.log > /dev/null 2>&1
+    if [ "$?" -eq "0" ]; then
+      break;
+    fi
+    echo "zzzzzz"
+    sleep 5
+done
+
 cd samples/axis2Client
 # run axis2 client forever
 while true
 do
-  ant stockquote -Dtrpurl=http://localhost:8280/services/StockQuote
-	sleep 5
+  case $1 in
+    5)
+      ant stockquote -Daddurl=http://localhost:9000/services/SimpleStockQuoteService -Dtrpurl=http://localhost:8280/ -Dsymbol=MSFT
+      ;;
+    *)
+      ant stockquote -Dtrpurl=http://localhost:8280/services/StockQuote
+      ;;
+  esac
+
+  if [ ! -z "$2" ]; then
+    sleep $2
+  else
+    exit 0
+  fi
 done
